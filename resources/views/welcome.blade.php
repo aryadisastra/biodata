f978de2f37b4b13016d09a696717b093a107393e
Windaapril <windaelpasha@gmailcom>
2019-10-29 09:32:47 +0700

    perubahan view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Holla!
                 </div>
 
                 <div class="links">
